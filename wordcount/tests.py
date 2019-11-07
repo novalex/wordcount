99ab32c2698d0bcb9eb2c978738181dc1e96c7ca
Alex Nitu <novalex.inc@gmail.com>
2019-11-07 12:26:25 +0000

    Added test cases for result listing functionality.

--- wordcount/tests.py
+++ wordcount/tests.py
@@ -82,3 +82,36 @@ class FileUploadTests(APITestCase):
         client = APIClient(HTTP_CONTENT_DISPOSITION='attachment; filename=testfile_failure')
         resp = client.post(self.url, 'ö', content_type='text/plain')
         self.assertEqual(resp.status_code, 400)
+
+    def test_list_all_results(self):
+        """
+        Test result listing functionality (GET method).
+        """
+        # Create test results in the DB
+        results = 3
+        client = APIClient(HTTP_CONTENT_DISPOSITION='attachment; filename=testfile_list')
+        i = 1
+        while i <= results:
+            client.post(self.url, 'test content %d' % i, content_type='text/plain')
+            i += 1
+
+        # List all results
+        client = APIClient()
+        resp = client.get(self.url)
+        # Assert that the correct number of results is returned
+        self.assertEqual(results, len(resp.data))
+
+    def test_list_single_result(self):
+        """
+        Test single result listing functionality (GET method with id query arg).
+        This test checks success and failure conditions.
+        """
+        # Create test result in DB
+        client = APIClient(HTTP_CONTENT_DISPOSITION='attachment; filename=testfile_single_list')
+        resp = client.post(self.url, 'test content', content_type='text/plain')
+        # Check listing of returned result id
+        resp = client.get(self.url + '?id=%d' % resp.data['id'])
+        self.assertEqual(resp.status_code, 200)
+        # Check listing non-existing result id
+        resp = client.get(self.url + '?id=999')
+        self.assertEqual(resp.status_code, 404)
